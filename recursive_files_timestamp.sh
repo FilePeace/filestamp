2e9cf5b89d0d297cb4c96651d3ca08c27e21b1ba
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-21 20:33:35 -0300

    "save" command - make it easier

--- recursive_files_timestamp.sh
+++ recursive_files_timestamp.sh
@@ -29,5 +29,8 @@ do
     #printf "\n"
     printf "$all"
 done
-
 # from https://stackoverflow.com/a/25948226/5623661
+
+if [ "$1" = "save" ];then
+   ./recursive_files_timestamp.sh > .file-timestamps
+fi
